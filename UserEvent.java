8da985669c6933d2cb9d5f626501c0de25e5a403
cobatake <cobatake@hawaii.edu>
2015-03-14 14:12:08 -1000

    Created getEvent and saveFile methods
    Added getEvent method to get user input for event
    - Added user input for end date in case event lasts over one day
    - Added specifics for user input for time and priority of event
    Added saveFile method to create .ics file for event
    main() calls to both of these methods

--- UserEvent.java
+++ UserEvent.java
@@ -17,38 +17,56 @@ public class UserEvent {
     public static void main (String args[]) throws IOException {
         Scanner scan = new Scanner(System.in);
         ICalendarEvent event = new ICalendarEvent();
-        System.out.print("Name of Event: ");
-        event.setSummary(scan.nextLine());
-        System.out.print("Location of Event: ");
-        event.setLocation(scan.nextLine());
-        System.out.print("Date of Event(yyyymmdd): ");
-        String month = scan.nextLine();
-        System.out.print("Start Time of Event(hhmmss): ");
-        String startTime = scan.nextLine();
-        event.setDTStart(month, startTime);
-        System.out.print("End Time of Event(hhmmss): ");
-        String endTime = scan.nextLine();
-        event.setDTEnd(month, endTime);
-        System.out.print("Timezone of Event: ");
-        event.setTimeZone(scan.nextLine());
-        System.out.print("Priority of Event: ");
-        event.setPriority(scan.nextLine());
-        System.out.print("Visibility of Event(public, private, confidential): ");
-        event.setClassification(scan.nextLine());
-        
-        FileWriter icsFile = new FileWriter(event.getSummary() + ".ics");
-        icsFile.write("BEGIN:VCALENDAR");
-        icsFile.write("\nVERSION:" + event.getVersion());
-        icsFile.write("\nBEGIN:VEVENT");
-        icsFile.write("\nCLASS:" + event.getClassification());
-        icsFile.write("\nLOCATION:" + event.getLocation());
-        icsFile.write("\nPRIORITY:" + event.getPriority());
-        icsFile.write("\nSUMMARY:" + event.getSummary());
-        icsFile.write("\nDTSTART;TZID=" + event.getTimeZone() + ":" + event.getDTStart());
-        icsFile.write("\nDTEND;TZID=" + event.getTimeZone() + ":" + event.getDTEnd());
-        icsFile.write("\nEND:VEVENT");
-        icsFile.write("\nEND:VCALENDAR");
-        
-        icsFile.close();
-    }
+		getEvent(scan, event);
+		saveFile(event);
+	}
+	
+	/**
+	 * Get event info
+	 * @param scan - user input
+	 * @param event - calendar event object
+	 */
+	public static void getEvent(Scanner scan, ICalendarEvent event) {
+		System.out.print("Name of Event: ");
+		event.setSummary(scan.nextLine().trim());
+		System.out.print("Location of Event: ");
+		event.setLocation(scan.nextLine().trim());
+		System.out.print("Start date of Event (yyyymmdd): ");
+		String startmonth = scan.nextLine().trim();
+		System.out.print("Start Time of Event in military time (hhmmss): ");
+		String startTime = scan.nextLine().trim();
+		event.setDTStart(startmonth, startTime);
+		System.out.print("End date of Event (yyyymmdd): ");
+		String endmonth = scan.nextLine().trim();
+		System.out.print("End Time of Event in military time (hhmmss): ");
+		String endTime = scan.nextLine().trim();
+		event.setDTEnd(endmonth, endTime);
+		System.out.print("Timezone of Event: ");
+		event.setTimeZone(scan.nextLine().trim());
+		System.out.print("Priority of Event (low, medium, high): ");
+		event.setPriority(scan.nextLine().trim());
+		System.out.print("Visibility of Event (public, private, confidential): ");
+		event.setClassification(scan.nextLine().trim());
+	}
+	
+	/**
+	 * Save calendar event to a .ics file
+	 * @param event - calendar event object
+	 */
+	public static void saveFile(ICalendarEvent event) throws IOException {
+		FileWriter icsFile = new FileWriter(event.getSummary() + ".ics");
+		icsFile.write("BEGIN:VCALENDAR");
+		icsFile.write("\nVERSION:" + event.getVersion());
+		icsFile.write("\nBEGIN:VEVENT");
+		icsFile.write("\nCLASS:" + event.getClassification());
+		icsFile.write("\nLOCATION:" + event.getLocation());
+		icsFile.write("\nPRIORITY:" + event.getPriority());
+		icsFile.write("\nSUMMARY:" + event.getSummary());
+		icsFile.write("\nDTSTART;TZID=" + event.getTimeZone() + ":" + event.getDTStart());
+		icsFile.write("\nDTEND;TZID=" + event.getTimeZone() + ":" + event.getDTEnd());
+		icsFile.write("\nEND:VEVENT");
+		icsFile.write("\nEND:VCALENDAR");
+		icsFile.close();
+		System.out.print("\nThe .ics file for your event has been created.");
+	}
 }
