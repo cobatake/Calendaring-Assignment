23a9c3f37bbd8007c83e058b3ae3d84a3223c6b1
chanelsantiago <chanels@hawaii.edu>
2015-04-03 23:57:13 -1000

    created method to find free time in a day, given a file that lists .ics files for that day.

--- ICalendarEvent.java
+++ ICalendarEvent.java
@@ -3,7 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -90,5 +102,99 @@ public class ICalendarEvent {
     }
     public String getTimeZone() {
         return timeZone;
+    }
+    public void findFreeTime(String filename) {
+        //times to compare with busy times...
+        String startOfDay = "000000";
+        String endOfDay = "235959";
+        
+        //read file of list of .ics files
+        File file1 = new File(filename);
+        Scanner readFile = null;
+        try {
+            readFile = new Scanner(file1);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ICalendarEvent.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Scanner readTemp = null;
+        String start = "000000";
+        String end = "235959";
+        String day = "20000101";
+        String tz = "";
+        List<String> times = new ArrayList<String>();
+        //Find start and end times for each event and add to arraylist.
+        //Assumption: all events occur on same day, no checks
+        while (readFile.hasNextLine()) {
+            String line = readFile.nextLine();
+            File tempFile = new File(line);
+            try {
+                readTemp = new Scanner(tempFile);
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(ICalendarEvent.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            while (readTemp.hasNextLine()) {
+                String tempLine = readTemp.nextLine();
+                if (tempLine.contains("DTSTART")) {
+                    start = tempLine.substring(tempLine.length()-6);
+                    day = tempLine.substring(tempLine.length()-15, tempLine.length()-6);
+                    tz = tempLine.substring(tempLine.indexOf("=") + 1, tempLine.indexOf(":"));
+                }
+                else if (tempLine.contains("DTEND")) {
+                    end = tempLine.substring(tempLine.length()-6);
+                }
+                
+            }
+            times.add(start);
+            times.add(end);
+        }
+        //Sort times (assuming no events overlap)
+        Collections.sort(times);
+        
+        //Find free time between events
+        String freeStart = "";
+        String freeEnd = "";
+        List<String> freetimes = new ArrayList<String>();
+        for (int i = 0; i <= times.size(); i++) {
+            if (i == times.size()) {
+                freeStart = startOfDay;
+                freeEnd = endOfDay;
+                i = times.size();
+            }
+            else {
+                freeStart = startOfDay;
+                freeEnd = times.get(i);
+                startOfDay = times.get(i+1);
+            }
+            freetimes.add(freeStart);
+            freetimes.add(freeEnd);
+            i++;
+        }
+        
+        //Create .ics files for "free time"
+        for (int i = 0; i < freetimes.size()-1; i++) {
+            FileWriter icsFile = null;
+            try {
+                icsFile = new FileWriter("freetime" + i/2 + ".ics");
+                icsFile.write("BEGIN:VCALENDAR");
+                icsFile.write("\nVERSION:" + this.version);
+                icsFile.write("\nBEGIN:VEVENT");
+                icsFile.write("\nCLASS:public");
+                icsFile.write("\nSUMMARY:Free Time");
+                icsFile.write("\nDTSTART;TZID=" + tz + ":" + day + freetimes.get(i));
+                icsFile.write("\nDTEND;TZID=" + tz + ":" + day + freetimes.get(i+1));
+                icsFile.write("\nEND:VEVENT");
+                icsFile.write("\nEND:VCALENDAR");
+                i++;
+            } catch (IOException ex) {
+                Logger.getLogger(ICalendarEvent.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                try {
+                    icsFile.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(ICalendarEvent.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        
+        }
     }
 }
--- UserEvent.java
+++ UserEvent.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -12,43 +13,52 @@ import java.util.Scanner;
  * @author chanelsantiago
  */
 public class UserEvent {
-    static String[] timezone1 = {"Africa", "America", "Antarctic", "Atlantic", "Europe", "Indian", "Pacific"};
-    static String[] africa = {"Abidjan", "Accra", "Addis_Ababa", "Algiers", "Asmara"};
     public static void main (String args[]) throws IOException {
         Scanner scan = new Scanner(System.in);
-        ICalendarEvent event = new ICalendarEvent();
-        System.out.print("Name of Event: ");
-        event.setSummary(scan.nextLine());
-        System.out.print("Location of Event: ");
-        event.setLocation(scan.nextLine());
-        System.out.print("Date of Event(yyyymmdd): ");
-        String month = scan.nextLine();
-        System.out.print("Start Time of Event(hhmmss): ");
-        String startTime = scan.nextLine();
-        event.setDTStart(month, startTime);
-        System.out.print("End Time of Event(hhmmss): ");
-        String endTime = scan.nextLine();
-        event.setDTEnd(month, endTime);
-        System.out.print("Timezone of Event: ");
-        event.setTimeZone(scan.nextLine());
-        System.out.print("Priority of Event: ");
-        event.setPriority(scan.nextLine());
-        System.out.print("Visibility of Event(public, private, confidential): ");
-        event.setClassification(scan.nextLine());
+        System.out.println("Enter # for option: ");
+        System.out.print("(1) Create new event or (2) Check free time available: ");
+        int choice = Integer.parseInt(scan.nextLine());
+        if (choice == 1) {
+            ICalendarEvent event = new ICalendarEvent();
+            System.out.print("Name of Event: ");
+            event.setSummary(scan.nextLine());
+            System.out.print("Location of Event: ");
+            event.setLocation(scan.nextLine());
+            System.out.print("Date of Event(yyyymmdd): ");
+            String month = scan.nextLine();
+            System.out.print("Start Time of Event(hhmmss): ");
+            String startTime = scan.nextLine();
+            event.setDTStart(month, startTime);
+            System.out.print("End Time of Event(hhmmss): ");
+            String endTime = scan.nextLine();
+            event.setDTEnd(month, endTime);
+            System.out.print("Timezone of Event: ");
+            event.setTimeZone(scan.nextLine());
+            System.out.print("Priority of Event: ");
+            event.setPriority(scan.nextLine());
+            System.out.print("Visibility of Event(public, private, confidential): ");
+            event.setClassification(scan.nextLine());
         
-        FileWriter icsFile = new FileWriter(event.getSummary() + ".ics");
-        icsFile.write("BEGIN:VCALENDAR");
-        icsFile.write("\nVERSION:" + event.getVersion());
-        icsFile.write("\nBEGIN:VEVENT");
-        icsFile.write("\nCLASS:" + event.getClassification());
-        icsFile.write("\nLOCATION:" + event.getLocation());
-        icsFile.write("\nPRIORITY:" + event.getPriority());
-        icsFile.write("\nSUMMARY:" + event.getSummary());
-        icsFile.write("\nDTSTART;TZID=" + event.getTimeZone() + ":" + event.getDTStart());
-        icsFile.write("\nDTEND;TZID=" + event.getTimeZone() + ":" + event.getDTEnd());
-        icsFile.write("\nEND:VEVENT");
-        icsFile.write("\nEND:VCALENDAR");
+            FileWriter icsFile = new FileWriter(event.getSummary() + ".ics");
+            icsFile.write("BEGIN:VCALENDAR");
+            icsFile.write("\nVERSION:" + event.getVersion());
+            icsFile.write("\nBEGIN:VEVENT");
+            icsFile.write("\nCLASS:" + event.getClassification());
+            icsFile.write("\nLOCATION:" + event.getLocation());
+            icsFile.write("\nPRIORITY:" + event.getPriority());
+            icsFile.write("\nSUMMARY:" + event.getSummary());
+            icsFile.write("\nDTSTART;TZID=" + event.getTimeZone() + ":" + event.getDTStart());
+            icsFile.write("\nDTEND;TZID=" + event.getTimeZone() + ":" + event.getDTEnd());
+            icsFile.write("\nEND:VEVENT");
+            icsFile.write("\nEND:VCALENDAR");
         
-        icsFile.close();
+            icsFile.close();
+        }
+        if (choice == 2) {
+            ICalendarEvent freetime = new ICalendarEvent();
+            System.out.print("Enter .txt file name that contains .ics file names: ");
+            String filename = scan.nextLine();
+            freetime.findFreeTime(filename);
+        }
     }
 }
